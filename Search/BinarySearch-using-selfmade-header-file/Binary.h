1a07af21950eb340d2a13843b2eede8017442045
Aditya Prasad Mishra <66214801+adimishrax7x@users.noreply.github.com>
2020-10-26 23:09:46 +0530

    Create Binary.h

---
+++ Search/BinarySearch-using-selfmade-header-file/Binary.h
@@ -0,0 +1,23 @@
+int Bsearch(int A[], int size, int element) 
+{
+   int start = 0;
+   int end = size-1;
+   
+   while(start<=end) 
+   {
+      int mid = (start+end)/2;
+      
+      if( A[mid] == element) 
+         return mid;
+         
+      else if( element < A[mid] ) 
+         end= mid-1;
+         
+      else if( element > A[mid] ) 
+         start = mid+1;
+
+   }
+   
+   return -1;
+   
+}
